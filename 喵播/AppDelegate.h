93ca0008131d13bd943aff2a19f202891f22be3c
mac <mac@hufeideimac.local>
2017-05-09 18:10:44 +0800

    Initial Commit

---
+++ 喵播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  喵播
+//
+//  Created by mac on 2017/5/9.
+//  Copyright © 2017年 com.hygl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
